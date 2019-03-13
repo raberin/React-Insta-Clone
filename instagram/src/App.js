c1fc68fa8b468d4c5076c06a338b2620ebc7490c
raberin <rmvaberin@gmail.com>
2019-03-12 21:16:12 -0700

    Added CSS Reset, Completed code for App.js to render the SearchBar and PostContainer

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,25 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "../../dummy-data";
+import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      dummyData: dummyData
+    };
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="App container">
+        <SearchBar />
+        <PostContainer dummData={this.state.dummyData} />
       </div>
     );
   }
