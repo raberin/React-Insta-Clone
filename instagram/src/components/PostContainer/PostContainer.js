08cfe6afc7ee83b1521596573d8171627a9eb231
raberin <rmvaberin@gmail.com>
2019-03-14 19:23:10 -0700

    Refined searchBar CSS

--- instagram/src/components/PostContainer/PostContainer.js
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -7,8 +7,8 @@ const PostContainer = props => {
   return props.dummyData.map(post => (
     <div className="post-container">
       <h2>{post.username}</h2>
-      <img src={post.thumbnailUrl} />
-      <img src={post.imageUrl} />
+      <img src={post.thumbnailUrl} alt="profile" />
+      <img src={post.imageUrl} alt="instagram post" />
       <p>{post.likes}</p>
       <p>{post.timestamp}</p>
       <CommentSection dummyDataComments={post.comments} />
--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -8,14 +8,16 @@ const SearchBar = props => {
         <img
           className="searchIcons"
           src="https://img.icons8.com/windows/32/000000/instagram-new.png"
-          height="24px"
-          width="24px"
+          height="29px"
+          width="29px"
+          alt="instagram logo"
         />
         <div className="dash" />
         <img
           src="https://soundwave.co.uk/cms/resources/blog/top-tips-on-maximising-your-instagram-obsession-ndash-and-making-it-work-for-your-business-430.jpg"
-          height="29px"
+          height="35px"
           width="103px"
+          alt="instagram"
         />
       </div>
 
@@ -26,6 +28,7 @@ const SearchBar = props => {
             src="https://img.icons8.com/ios/32/000000/search-filled.png"
             height="10px"
             width="10px"
+            alt="magnify glass"
           />
           <p>Search</p>
         </div>
@@ -35,20 +38,23 @@ const SearchBar = props => {
         <img
           className="searchIcons"
           src="https://img.icons8.com/windows/32/000000/compass.png"
-          height="24px"
-          width="24px"
+          height="29px"
+          width="29px"
+          alt="compass icon"
         />
         <img
           className="searchIcons"
           src="https://img.icons8.com/windows/32/000000/hearts.png"
-          height="24px"
-          width="24px"
+          height="29px"
+          width="29px"
+          alt="heart icon"
         />
         <img
           className="searchIcons"
           src="https://img.icons8.com/windows/32/000000/contacts.png"
-          height="24px"
-          width="24px"
+          height="29px"
+          width="29px"
+          alt="contacts icon"
         />
       </div>
     </div>
