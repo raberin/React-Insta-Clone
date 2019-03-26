05a01f0cc5d670fb5c7216b9a8db004be450f88f
raberin <rmvaberin@gmail.com>
2019-03-25 21:56:14 -0700

    Added styled components on necessary components

--- instagram/src/components/CommentSection/Comment.js
+++ instagram/src/components/CommentSection/Comment.js
@@ -1,13 +1,18 @@
 import React from "react";
 import "./CommentSection.css";
+import styled from "styled-components";
+
+const CommentDiv = styled.div`
+  margin: 1.5% 1.5%;
+`;
 
 const Comment = props => {
   return (
-    <div className="comment">
+    <CommentDiv>
       <p>
         <strong>{props.username}</strong> {props.text}
       </p>
-    </div>
+    </CommentDiv>
   );
 };
 
--- instagram/src/components/CommentSection/CommentInput.js
+++ instagram/src/components/CommentSection/CommentInput.js
@@ -1,12 +1,42 @@
 import React from "react";
 import "../PostContainer/PostContainer.css";
+import styled from "styled-components";
+
+const AddCommentSection = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  margin-top: 1.2rem;
+`;
+
+const AddCommentDiv = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  align-content: center;
+`;
+
+const AddCommentForm = styled.form`
+  width: 95%;
+  margin-left: 1.5%;
+  height: 3rem;
+  font-size: 1.6rem;
+`;
+
+const ThreeDotsIconDiv = styled.div`
+  width: 5%;
+  display: flex;
+  justify-content: flex-end;
+  margin-right: 1.5%;
+`;
 
 const CommentInput = props => {
   return (
-    <div className="addCommentSection">
+    <AddCommentSection>
       <div className="borderTop" />
-      <div className="addCommentDiv">
-        <form onSubmit={props.addNewComment}>
+      <AddCommentDiv>
+        <AddCommentForm onSubmit={props.addNewComment}>
           <input
             className="addCommentInput"
             type="text"
@@ -14,18 +44,18 @@ const CommentInput = props => {
             name="Input Field"
             onChange={props.inputChangeHandler}
           />
-        </form>
+        </AddCommentForm>
 
-        <div className="threeDotsIcon">
+        <ThreeDotsIconDiv className="threeDotsIcon">
           <img
             alt="three dots"
             src="https://img.icons8.com/windows/32/000000/more.png"
             height="35px"
             width="35px"
           />
-        </div>
-      </div>
-    </div>
+        </ThreeDotsIconDiv>
+      </AddCommentDiv>
+    </AddCommentSection>
   );
 };
 
