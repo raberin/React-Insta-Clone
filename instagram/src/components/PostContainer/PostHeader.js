cb65c6f7855b52bd272e809f31782a3530c5afc2
raberin <rmvaberin@gmail.com>
2019-03-25 21:35:21 -0700

    PostHeader Styled components - Done

--- instagram/src/components/PostContainer/PostHeader.js
+++ instagram/src/components/PostContainer/PostHeader.js
@@ -1,21 +1,35 @@
 import React from "react";
-import "./PostContainer.css";
+import styled from "styled-components";
+
+const PicAndUsernameDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-content: center;
+  align-items: center;
+  margin-left: 1.5rem;
+  margin: 2% 1.5%;
+`;
+
+const ProfilePic = styled.img`
+  border-radius: 20px;
+  margin-right: 10px;
+  height: 35px;
+  width: 35px;
+`;
 
 const PostHeader = props => {
   return (
     <div className="post-header">
-      <div className="picAndUsername">
-        <img
+      <PicAndUsernameDiv>
+        <ProfilePic
           className="profilePic"
           src={props.thumbnailUrl}
           alt="profile"
-          height="35px"
-          width="35px"
         />
         <h2>
           <strong>{props.username}</strong>
         </h2>
-      </div>
+      </PicAndUsernameDiv>
     </div>
   );
 };
